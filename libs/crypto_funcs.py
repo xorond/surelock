65e813bbac8546bc0af67d2c1be3ccfacc541615
xorond <xorond@gmail.com>
2019-05-27 02:21:17 +0200

    fix up aes encryption/decryption, introduce object orientation

--- libs/crypto_funcs.py
+++ libs/crypto_funcs.py
@@ -4,11 +4,30 @@ import base64
 import hashlib
 from Crypto import Random
 from Crypto.Cipher import AES
-#from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Protocol.KDF import PBKDF2
 
-pad = lambda s: s + (AES.block_size - len(s) % AES.block_size) * chr(AES.block_size
-        - len(s) % AES.block_size)
-unpad = lambda s: s[:-ord(s[len(s) - 1:])] # TODO: fix this
+pad = lambda s: s + (AES.block_size - len(s) % AES.block_size) * chr(AES.block_size - len(s) % AES.block_size)
+unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+
+class Algorithm:
+    # AES-CBC with PBKDF2 derived salted password using 16 bit initialization
+    # vector
+    def __init__(self, password):
+        salt = b"we are salty"
+        kdf = PBKDF2(password, salt, 64, 1000)
+        self.key = kdf[:32]
+
+    def encrypt(self, raw):
+        raw = pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw.encode('utf8')))
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return unpad(cipher.decrypt(enc[16:])).decode('utf8')
 
 def get_pass_input(text="Type your passphrase: "):
     # text: The default text to show the user while asking for password
@@ -21,29 +40,16 @@ def get_pass_input(text="Type your passphrase: "):
     else:
         return secret
 
-def generate_key(password):
-    key = hashlib.sha256(password.encode("utf-8")).digest()
-    return key
+# For testing purposes
+if __name__ == '__main__':
+    text = input("text: ")
+    pwd = get_pass_input()
 
-def encrypt_text(text, password):
-    key = generate_key(password)
-    text = pad(text)
-    iv = Random.new().read(AES.block_size)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    return base64.b64encode(cipher.encrypt(text.encode()))
+    #enc = Algorithm(pwd).encrypt(msg)
+    #print("Ciphertext: {}".format(enc))
 
-def decrypt_text(enc, password):
-    key = generate_key(password)
-    enc = base64.b64decode(enc)
-    iv = enc[:16]
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    return unpad(cipher.decrypt(enc[16:]))
-
-'''
-# DEBUG
-password = get_pass_input()
-encrypted = encrypt_text("test encryption", password)
-print(encrypted)
-decrypted = decrypt_text(encrypted, password)
-print(bytes.decode(decrypted))
-'''
+    enc = text
+    try:
+        print("Decrypted: {}".format(Algorithm(pwd).decrypt(enc)))
+    except Exception as e:
+        print(f"Error: {e}")

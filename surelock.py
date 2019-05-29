d736a66aab43716025a48ca2abe1fb7175cf0ee4
xorond <xorond@gmail.com>
2019-05-29 03:31:43 +0200

    surelock: overhaul command line parser and implement list/init db

--- surelock.py
+++ surelock.py
@@ -6,46 +6,48 @@ import argparse
 
 def main():
     
-    parser = argparse.ArgumentParser(prog='Surelock')
+    parser = argparse.ArgumentParser(prog='surelock')
     subparsers = parser.add_subparsers(dest='subparser_name')
 
-    parser_enc = subparsers.add_parser('enc', help='enc help')
-    parser_enc.add_argument("-o","--outfile" , help="name of the encrypted file", type=str)
-    parser_enc.add_argument("-i","--infile" , help="name of the input file", type=str)
-    parser_enc.add_argument("-k","--keyfile" , help="name of the keyfile", type=str)
-    parser_enc.add_argument("-b","--bytes" , help="number of random bytes used for encryption", type=int)
+    parser_add = subparsers.add_parser('add', help='add a new entry')
+    parser_add.add_argument("entry", help="add an entry to the database", type=str, default="surelock.db")
 
-    parser_dec = subparsers.add_parser('dec', help='dec help')
-    parser_dec.add_argument("-o","--outfile" , help="name of the decrypted output file", type=str)
-    parser_dec.add_argument("-i","--infile" , help="name of the encrypted input file", type=str)
-    parser_dec.add_argument("-k","--keyfile" , help="name of the keyfile", type=str)
-    parser_dec.add_argument("-b","--bytes" , help="number of random bytes used for encryption", type=int)
+    parser_ret= subparsers.add_parser('view', help='view an entry')
+    parser_ret.add_argument("entry", help="view an entry from the database", type=str, default="surelock.db")
 
-    parser_add = subparsers.add_parser('add', help='add help')
-    parser_add.add_argument("entry", help="add an entry to the database", type=str)
+    parser_rem = subparsers.add_parser('del', help='delete an entry')
+    parser_rem.add_argument("entry", help="delete an entry from the database", type=str, default="surelock.db")
 
-    parser_ret= subparsers.add_parser('ret', help='ret help')
-    parser_ret.add_argument("entry", help="retrieve an entry from the database", type=str)
-
-    parser_rem = subparsers.add_parser('del', help='del help')
-    parser_rem.add_argument("entry", help="delete an entry from the database", type=str)
+    parser_list = subparsers.add_parser('list', help='list categories from the database')
+    parser_list.add_argument("-f","--file" , help="name of the database file", type=str, default="surelock.db")
     
-#    parser_init = subparsers.add_parser('init')
+    parser_init = subparsers.add_parser('init', help='initialize the database')
+    parser_init.add_argument("-f","--file" , help="name of the database file", type=str, default="surelock.db")
 
     args = parser.parse_args()
 
-    if args.subparser_name == 'enc':
-        encrypt_file(args.infile, args.outfile, args.keyfile, args.bytes)
-        print(args.infile)
-    if args.subparser_name == 'dec':
-        decrypt_file(args.infile, args.outfile, args.keyfile, args.bytes)
+    if args.subparser_name == 'init':
+        db = sql.Database(filename=args.file)
+        sql.init_database(db)
+
+    if args.subparser_name == 'list':
+        db = sql.Database(filename=args.file)
+        tables = sql.list_tables(db)
+        print(tables)
+
+# TODO: all of these
+
 #    if args.subparser_name == 'add':
+#        db = sql.Database(filename=args.file)
         
 #    if args.subparser_name == 'ret':
-        
+#        db = sql.Database(filename=args.file)
+
 #    if args.subparser_name == 'del':
+#        db = sql.Database(filename=args.file)
         
 #    if args.subparser_name == 'init':
+#        db = sql.Database(filename=args.file)
 
 if __name__ == '__main__':
     main()

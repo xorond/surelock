e76fe857fe44a789f3b4b79ebddcfee982c86b15
xorond <xorond@gmail.com>
2019-05-29 03:30:59 +0200

    libs/sql: modify functions to take db object as argument

--- libs/sql.py
+++ libs/sql.py
@@ -20,42 +20,43 @@ class Database:
     def close(self):
         self.conn.close()
 
-db = Database()
-
-def create_table(table_name, filename=db_name):
+def create_table(db, table_name, filename=db_name):
     db.run_cmd(f""" CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY AUTOINCREMENT, site TEXT, password TEXT, description TEXT) """)
     db.commit()
 
-def insert_entry(entry_name, entry, description='', rowid='', table_name='root', filename=db_name):
-    # use REPLACE if a rowid is given, INSERT if not
+def insert_entry(db, entry_name, entry, description='', rowid='', table_name='root', filename=db_name):
+    # db: database object
+
     # TODO: implement encrypt_text and decrypt_text in this function
+
     if rowid == '':
         db.run_cmd(f""" INSERT INTO {table_name} (site, password, description) VALUES ('{entry_name}', '{entry}', '{description}') """)
         db.commit()
     else:
         db.run_cmd(f""" REPLACE INTO {table_name} (id, site, password, description) VALUES ('{rowid}', '{entry_name}', '{entry}', '{description}') """)
+        # we use REPLACE if a rowid is given, INSERT if not
         db.commit()
 
-def delete_entry(rowid, table_name='root'):
+def delete_entry(db, rowid, table_name='root'):
     db.run_cmd(f""" DELETE FROM {table_name} WHERE id = {rowid}""")
     db.commit()
 
-def init_database(filename=db_name):
-    create_table('root', filename)
+def init_database(db, filename=db_name):
+    create_table(db, 'root', filename)
     db.commit()
 
-def list_tables(filename=db_name):
+def list_tables(db, filename=db_name):
     db.run_cmd("SELECT name FROM sqlite_master WHERE type='table'")
     tables = db.get_cursor().fetchall()
     db.commit()
     return tables
 
-def retrieve_table(table_name, filename=db_name):
+def retrieve_table(db, table_name, filename=db_name):
     # TODO: like in list_tables
     db.run_cmd(f""" SELECT * FROM {table_name}""")
     db.commit()
     
-def retrieve_entry(site, table_name, filename=db_name):
+def retrieve_entry(db, site, table_name, filename=db_name):
     # TODO: like in list_tables
     db.run_cmd(f""" SELECT * FROM {table_name} where site={site}""")
     db.commit()

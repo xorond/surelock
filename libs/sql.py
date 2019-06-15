3ed7425e986b1dfa5b951b6dfc55ceec726bde4e
Alex6312 <panzenboeck.alexander@yahoo.de>
2019-06-15 07:12:48 +0200

    Update sql.py
    The sql Functions now allow for category names like '5' or 'Uni Wien'

--- libs/sql.py
+++ libs/sql.py
@@ -25,11 +25,11 @@ class Database:
 
 def create_table(db, table_name, filename=db_name):
     # db: database object
-    db.run_cmd(f""" CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY AUTOINCREMENT, site TEXT, password TEXT, description TEXT, username TEXT) """)
+    db.run_cmd(f""" CREATE TABLE IF NOT EXISTS '{table_name}' (id INTEGER PRIMARY KEY AUTOINCREMENT, site TEXT, password TEXT, description TEXT, username TEXT) """)
     db.commit()
 
 def delete_entry(db, site, table_name='root'):
-    db.run_cmd(f""" DELETE FROM {table_name} WHERE site='{site}' """)
+    db.run_cmd(f""" DELETE FROM '{table_name}' WHERE site='{site}' """)
     db.commit()
 
 def init_database(db, filename=db_name):
@@ -54,13 +54,13 @@ def list_tables_with_number_of_entries(db, filename=db_name):
     return tables_with_entries
 
 def retrieve_table(db, table_name='root', filename=db_name):
-    db.run_cmd(f""" SELECT site, description, username FROM {table_name}""")
+    db.run_cmd(f""" SELECT site, description, username FROM '{table_name}'""")
     table = db.get_cursor().fetchall()
     db.commit()
     return table
 
 def retrieve_entry(db, pwd, site, table_name='root', filename=db_name):
-    db.run_cmd(f""" SELECT password FROM {table_name} WHERE site='{site}' """)
+    db.run_cmd(f""" SELECT password FROM '{table_name}' WHERE site='{site}' """)
     entrypwd = db.get_cursor().fetchall()
     a=entrypwd[0][0]
     decpwd = crypto_funcs.Algorithm(pwd).decrypt(a)
@@ -72,14 +72,14 @@ def insert_entry(db, pwd, entry_name, entry_password, description='', table_name
     entry_password = crypto_funcs.Algorithm(pwd).encrypt(entry_password).decode("ascii")
     a=retrieve_entries(db, table_name, filename)
     if (entry_name,) not in a:
-        db.run_cmd(f""" INSERT INTO {table_name} (site, password, description, username) VALUES ('{entry_name}', '{entry_password}', '{description}', '{username}') """)
+        db.run_cmd(f""" INSERT INTO '{table_name}' (site, password, description, username) VALUES ('{entry_name}', '{entry_password}', '{description}', '{username}') """)
         db.commit()
     else:
         c=input("Do you want to replace username, password and description for this entry? y/n")
         if ask_confirmation(c):
             a=get_rowid(db, entry_name, table_name)
             delete_entry(db, entry_name, table_name)
-            db.run_cmd(f""" INSERT INTO {table_name} (id, site, password, description, username) VALUES ('{a}', '{entry_name}', '{entry_password}', '{description}', '{username}') """)
+            db.run_cmd(f""" INSERT INTO '{table_name}' (id, site, password, description, username) VALUES ('{a}', '{entry_name}', '{entry_password}', '{description}', '{username}') """)
             db.commit()
             
 def insert_entry_gui(db, pwd, entry_name, entry_password, description='', table_name='root', filename=db_name, username=''):
@@ -87,29 +87,29 @@ def insert_entry_gui(db, pwd, entry_name, entry_password, description='', table_
     entry_password = crypto_funcs.Algorithm(pwd).encrypt(entry_password).decode("ascii")
     a=retrieve_entries(db, table_name, filename)
     if (entry_name,) not in a:
-        db.run_cmd(f""" INSERT INTO {table_name} (site, password, description, username) VALUES ('{entry_name}', '{entry_password}', '{description}', '{username}') """)
+        db.run_cmd(f""" INSERT INTO '{table_name}' (site, password, description, username) VALUES ('{entry_name}', '{entry_password}', '{description}', '{username}') """)
         db.commit()
     else:
         a=get_rowid(db, entry_name, table_name)
         delete_entry(db, entry_name, table_name)
-        db.run_cmd(f""" INSERT INTO {table_name} (id, site, password, description, username) VALUES ('{a}', '{entry_name}', '{entry_password}', '{description}', '{username}') """)
+        db.run_cmd(f""" INSERT INTO '{table_name}' (id, site, password, description, username) VALUES ('{a}', '{entry_name}', '{entry_password}', '{description}', '{username}') """)
         db.commit()
 
 def retrieve_entries(db, table_name='root', filename=db_name):
-    db.run_cmd(f""" SELECT site FROM {table_name}""")
+    db.run_cmd(f""" SELECT site FROM '{table_name}'""")
     entries = db.get_cursor().fetchall()
     db.commit()
     return entries
 
 def get_rowid(db, site, table_name='root'):
-    db.run_cmd(f""" SELECT rowid FROM {table_name} WHERE site='{site}' """)
+    db.run_cmd(f""" SELECT rowid FROM '{table_name}' WHERE site='{site}' """)
     i = db.get_cursor().fetchall()
     rowid=i[0][0]
     db.commit()
     return rowid
 
 def delete_table(db, table_name, filename=db_name):
-    db.run_cmd(f""" DROP TABLE IF EXISTS {table_name} """)
+    db.run_cmd(f""" DROP TABLE IF EXISTS '{table_name}' """)
     db.commit()
 
 def ask_confirmation(answer):
